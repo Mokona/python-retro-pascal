e9dfc0f0d2a84023f8fe77dbceb99d478b0afc14
Sylvain Glaize <mokona@puupuu.org>
2023-05-24 00:35:42 +0200

    Move assembling for a line into a new file.

---
+++ reinterpreted/assembler.py
@@ -0,0 +1,104 @@
+import unittest
+
+from translation import string_buffer
+
+# Taken from P2, not really fitting the Python environment on a current PC
+LARGEINT = 524288  # (* = 2**19 *)
+
+# Instructions
+instructions = ['LOD', 'LDO', 'STR', 'SRO', 'LDA', 'LAO', 'STO', 'LDC', '...', 'IND',
+                'INC', 'MST', 'CUP', 'ENT', 'RET', 'CSP', 'IXA', 'EQU', 'NEQ', 'GEQ',
+                'GRT', 'LEQ', 'LES', 'UJP', 'FJP', 'XJP', 'CHK', 'EOF', 'ADI', 'ADR',
+                'SBI', 'SBR', 'SGS', 'FLT', 'FLO', 'TRC', 'NGI', 'NGR', 'SQI', 'SQR',
+                'ABI', 'ABR', 'NOT', 'AND', 'IOR', 'DIF', 'INT', 'UNI', 'INN', 'MOD',
+                'ODD', 'MPI', 'MPR', 'DVI', 'DVR', 'MOV', 'LCA', 'DEC', 'STP']
+
+# Standard functions are procedures
+sptable = ['GET', 'PUT', 'RST', 'RLN', 'NEW',
+           'WLN', 'WRS', 'ELN', 'WRI', 'WRR',
+           'WRC', 'RDI', 'RDR', 'RDC', 'SIN',
+           'COS', 'EXP', 'LOG', 'SQT', 'ATN',
+           'SAV']
+
+
+def assemble(get_name, label_search, line, pc, store):
+    """TRANSLATE SYMBOLIC CODE INTO MACHINE CODE AND context.store"""
+    name, line = get_name(line)
+
+    op = instructions.index(name)
+    p = 0
+    q = 0
+
+    if op in (17, 18, 19, 20, 21, 22):  # (*EQU,NEQ,GEQ,GRT,LEQ,LES*)
+        p = [0, 1, 2, 3, 4, 5]['AIRBSM'.index(line[0])]
+        if p == 5:
+            q, _ = string_buffer.parse_integer(line[1:])
+    elif op in (0, 2, 4):  # (*LOD,STR,LDA*)
+        p, line = string_buffer.parse_integer(line)
+        q, _ = string_buffer.parse_integer(line)
+    elif op == 12:  # (*CUP*)
+        p, line = string_buffer.parse_integer(line)
+        q = label_search(pc, line)
+    elif op == 11:  # (*MST*)
+        p, _ = string_buffer.parse_integer(line)
+    elif op == 14:  # (*RET*)
+        p = [0, 1, 2, 3, 4, 5]['PIRCBA'.index(line[0])]
+    elif op in (1, 3, 5, 9, 10, 16, 55, 57):  # (*LDO,SRO,LAO,IND,INC,IXA,MOV,DEC*)
+        q, _ = string_buffer.parse_integer(line)
+    elif op in (13, 23, 24, 25):  # (*ENT,UJP,FJP,XJP*)
+        q = label_search(pc, line)
+    elif op == 15:  # (*CSP*)
+        name, _ = get_name(line)
+        while sptable[q] != name:
+            q += 1
+    elif op == 7:  # (*LDC*)
+        type_ch = line[0]
+        line = line[1:]
+        if type_ch == 'I':
+            p = 1
+            i, _ = string_buffer.parse_integer(line)
+            if abs(i) > LARGEINT:
+                op = 8  # Change to LCI
+                q = store.add_int_constant(i)
+            else:
+                q = i
+            pass
+        elif type_ch == 'R':
+            op = 8  # Change to LCI
+            p = 2
+            r, _ = string_buffer.parse_real(line)
+            q = store.add_real_constant(r)
+            pass
+        elif type_ch == 'N':
+            pass
+        elif type_ch == 'B':
+            p = 3
+            q, _ = string_buffer.parse_integer(line)
+        elif type_ch == '(':
+            op = 8  # Change to LCI
+            p = 4
+            s = set()
+
+            while type_ch != ')':
+                s1, line = string_buffer.parse_integer(line)
+                type_ch = line.strip()[0]
+
+                s.add(s1)
+
+            q = store.add_set_constant(s)
+    elif op == 26:  # (*CHK*)
+        lb, line = string_buffer.parse_integer(line[1:])
+        ub, _ = string_buffer.parse_integer(line)
+
+        store.add_boundary_constant((lb, ub))
+    elif op == 56:  # (*LCA*)
+        line = line[1:]
+        data = [ord(ch) for ch in line[:line.index("'")]]
+        q = store.add_multiple_constant(data)
+
+    return op, p, q
+
+
+class TestAssembler(unittest.TestCase):
+    def test_one(self):
+        pass
--- reinterpreted/pascal_interpreter.py
+++ reinterpreted/pascal_interpreter.py
@@ -2,6 +2,7 @@ import io
 from math import sin, cos, exp, log, sqrt, atan, trunc
 from os.path import splitext
 
+from reinterpreted.assembler import assemble
 from reinterpreted.store import Store, StoreConfiguration
 from translation import streams, string_buffer
 
@@ -22,7 +23,6 @@ OVERS: int = OVERR + 70  # (* SIZE OF SET CONSTANT TABLE = 70 *)
 OVERB: int = OVERS + 4  # (* SIZE OF BOUNDARY CONSTANT TABLE = 4 *)
 OVERM: int = OVERB + 1300  # (* SIZE OF MULTIPLE CONSTANT TABLE = 1300 *)
 MAXSTR = OVERM + 1  #
-LARGEINT = 524288  # (* = 2**19 *)
 BEGINCODE = 3  #
 INPUTADR = 4  # (* ABSOLUTE ADDRESS *)
 OUTPUTADR = 5  #
@@ -31,23 +31,6 @@ PRRADR = 7  #
 
 MAXLABEL = 1550
 
-# Instructions
-instructions = ['LOD', 'LDO', 'STR', 'SRO', 'LDA', 'LAO', 'STO', 'LDC', '...', 'IND',
-                'INC', 'MST', 'CUP', 'ENT', 'RET', 'CSP', 'IXA', 'EQU', 'NEQ', 'GEQ',
-                'GRT', 'LEQ', 'LES', 'UJP', 'FJP', 'XJP', 'CHK', 'EOF', 'ADI', 'ADR',
-                'SBI', 'SBR', 'SGS', 'FLT', 'FLO', 'TRC', 'NGI', 'NGR', 'SQI', 'SQR',
-                'ABI', 'ABR', 'NOT', 'AND', 'IOR', 'DIF', 'INT', 'UNI', 'INN', 'MOD',
-                'ODD', 'MPI', 'MPR', 'DVI', 'DVR', 'MOV', 'LCA', 'DEC', 'STP']
-
-# Standard functions are procedures
-sptable = ['GET', 'PUT', 'RST', 'RLN', 'NEW',
-           'WLN', 'WRS', 'ELN', 'WRI', 'WRR',
-           'WRC', 'RDI', 'RDR', 'RDC', 'SIN',
-           'COS', 'EXP', 'LOG', 'SQT', 'ATN',
-           'SAV']
-
-
-# Global variables
 
 class Code:
     def __init__(self):
@@ -104,89 +87,6 @@ def load(prd, store: Store):
 
         return lookup(pc, x)
 
-    def assemble(line, pc, store):
-        """TRANSLATE SYMBOLIC CODE INTO MACHINE CODE AND context.store"""
-        name, line = get_name(line)
-
-        op = instructions.index(name)
-        p = 0
-        q = 0
-
-        if op in (17, 18, 19, 20, 21, 22):  # (*EQU,NEQ,GEQ,GRT,LEQ,LES*)
-            p = [0, 1, 2, 3, 4, 5]['AIRBSM'.index(line[0])]
-            if p == 5:
-                q, _ = string_buffer.parse_integer(line[1:])
-        elif op in (0, 2, 4):  # (*LOD,STR,LDA*)
-            p, line = string_buffer.parse_integer(line)
-            q, _ = string_buffer.parse_integer(line)
-        elif op == 12:  # (*CUP*)
-            p, line = string_buffer.parse_integer(line)
-            q = label_search(pc, line)
-        elif op == 11:  # (*MST*)
-            p, _ = string_buffer.parse_integer(line)
-        elif op == 14:  # (*RET*)
-            p = [0, 1, 2, 3, 4, 5]['PIRCBA'.index(line[0])]
-        elif op in (1, 3, 5, 9, 10, 16, 55, 57):  # (*LDO,SRO,LAO,IND,INC,IXA,MOV,DEC*)
-            q, _ = string_buffer.parse_integer(line)
-        elif op in (13, 23, 24, 25):  # (*ENT,UJP,FJP,XJP*)
-            q = label_search(pc, line)
-        elif op == 15:  # (*CSP*)
-            name, _ = get_name(line)
-            while sptable[q] != name:
-                q += 1
-        elif op == 7:  # (*LDC*)
-            type_ch = line[0]
-            line = line[1:]
-            if type_ch == 'I':
-                p = 1
-                i, _ = string_buffer.parse_integer(line)
-                if abs(i) > LARGEINT:
-                    op = 8  # Change to LCI
-                    q = store.add_int_constant(i)
-                else:
-                    q = i
-                pass
-            elif type_ch == 'R':
-                op = 8  # Change to LCI
-                p = 2
-                r, _ = string_buffer.parse_real(line)
-                q = store.add_real_constant(r)
-                pass
-            elif type_ch == 'N':
-                pass
-            elif type_ch == 'B':
-                p = 3
-                q, _ = string_buffer.parse_integer(line)
-            elif type_ch == '(':
-                op = 8  # Change to LCI
-                p = 4
-                s = set()
-
-                while type_ch != ')':
-                    s1, line = string_buffer.parse_integer(line)
-                    type_ch = line.strip()[0]
-
-                    s.add(s1)
-
-                q = store.add_set_constant(s)
-        elif op == 26:  # (*CHK*)
-            lb, line = string_buffer.parse_integer(line[1:])
-            ub, _ = string_buffer.parse_integer(line)
-
-            store.add_boundary_constant((lb, ub))
-        elif op == 56:  # (*LCA*)
-            line = line[1:]
-            data = [ord(ch) for ch in line[:line.index("'")]]
-            q = store.add_multiple_constant(data)
-
-        code[pc].op = op
-        code[pc].p = p
-        code[pc].q = q
-
-        pc += 1
-
-        return pc
-
     def update(label_id, label_value):
         nonlocal labeltab
 
@@ -226,7 +126,13 @@ def load(prd, store: Store):
                         label_value = pc
                     update(x, label_value)
                 elif ch == ' ':
-                    pc = assemble(line, pc, store)
+                    op, p, q = assemble(get_name, label_search, line, pc, store)
+                    code[pc].op = op
+                    code[pc].p = p
+                    code[pc].q = q
+
+                    pc += 1
+
             else:
                 break
 
