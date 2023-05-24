2d70ecbffabc7de14b4b984e041125213257f12f
Sylvain Glaize <mokona@puupuu.org>
2023-05-24 13:33:34 +0200

    Simplify the lookup chain in label management.

--- reinterpreted/asm_labels.py
+++ reinterpreted/asm_labels.py
@@ -5,36 +5,26 @@ from reinterpreted.code import Code
 
 class Labels:
     def __init__(self, max_labels):
-        self.labeltab = [(-1, 'ENTERED') for _ in range(max_labels + 1)]
+        self.labels = [(-1, 'ENTERED') for _ in range(max_labels + 1)]
 
     def add_reference(self, label_id, pc):
-        current_value, status = self.labeltab[label_id]
-
-        vq = -1
+        value, status = self.labels[label_id]
 
         if status == 'ENTERED':
             # The lookup of referenced by undefined labels create a chain using
             # the code 'q' parameters. The chain will be updated when the label
             # is declared.
-            if current_value == -1:
-                self.labeltab[label_id] = (pc, status)
-                vq = -1
-            else:
-                vq = current_value
-                self.labeltab[label_id] = (pc, status)
-        elif status == 'DEFINED':
-            # The address of a defined label is its value.
-            vq = current_value
+            self.labels[label_id] = (pc, status)
 
-        return vq
+        return value
 
     def declare(self, label_id, label_value, code):
-        value, status = self.labeltab[label_id]
+        value, status = self.labels[label_id]
         if status == 'DEFINED':
             raise RuntimeError("Duplicated label")
         else:
             if value != -1:  # Forward reference
-                current, _ = self.labeltab[label_id]
+                current, _ = self.labels[label_id]
                 end_list = False
                 while not end_list:
                     c = code[current]
@@ -45,7 +35,7 @@ class Labels:
                     else:
                         current = successor
 
-            self.labeltab[label_id] = (label_value, 'DEFINED')
+            self.labels[label_id] = (label_value, 'DEFINED')
 
 
 class TestLabels(unittest.TestCase):
