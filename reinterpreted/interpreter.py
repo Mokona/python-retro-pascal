5d2bd5880a3b943918d0c091b8e1f383e2035b6c
Sylvain Glaize <mokona@puupuu.org>
2023-05-24 21:40:21 +0200

    Get only value from store when the type is not needed.

--- reinterpreted/interpreter.py
+++ reinterpreted/interpreter.py
@@ -13,7 +13,7 @@ PRRADR = 7  #
 def base(context, ld):
     ad = context.mp
     while ld > 0:
-        _, ad = context.store[ad + 1]  # Read static link
+        ad = context.store.get_value(ad + 1)  # Read static link
         ld -= 1
     return ad
 
@@ -29,7 +29,7 @@ def get_stream(context, stream_id) -> streams.InputStream | io.IOBase:
 
 
 def getfile(context):
-    _, file_id = context.store[context.sp]
+    file_id = context.store.get_value(context.sp)
     if file_id in (5, 7):
         raise RuntimeError("Get on Output. Error")
 
@@ -39,15 +39,15 @@ def getfile(context):
 
 
 def putfile(context):
-    _, file_id = context.store[context.sp]
-    _, value = context.store[file_id]
+    file_id = context.store.get_value(context.sp)
+    value = context.store.get_value(file_id)
     get_stream(context, file_id).write(str([value]))
     context.store[file_id] = ('UNDEF', 0)
     context.sp -= 1
 
 
 def readline(context):
-    _, file_id = context.store[context.sp]
+    file_id = context.store.get_value(context.sp)
     stream = get_stream(context, file_id)
     stream.read_line()
     value = stream.read()
@@ -56,26 +56,26 @@ def readline(context):
 
 
 def writeline(context):
-    _, file_id = context.store[context.sp]
+    file_id = context.store.get_value(context.sp)
     stream = get_stream(context, file_id)
     stream.writelines(["\n"])
     context.sp -= 1
 
 
 def eoln(context):
-    _, file_id = context.store[context.sp]
+    file_id = context.store.get_value(context.sp)
     stream = get_stream(context, file_id)
     result = stream.eol()
     context.store[context.sp] = ('BOOL', result)
 
 
 def writestr(context: Context):
-    _, file_id = context.store[context.sp]
+    file_id = context.store.get_value(context.sp)
     stream = get_stream(context, file_id)
 
-    _, ad = context.store[context.sp - 3]
-    _, k = context.store[context.sp - 1]
-    _, j = context.store[context.sp - 2]
+    address = context.store.get_value(context.sp - 3)
+    k = context.store.get_value(context.sp - 1)
+    j = context.store.get_value(context.sp - 2)
 
     k, j = j, k  # I'm not sure why these two values seem inverted compared to original
     # If not inverted, this makes no sense. Or is it because another bug?
@@ -87,16 +87,16 @@ def writestr(context: Context):
         j = k
 
     for i in range(j):
-        _, v = context.store[ad + i]
+        v = context.store.get_value(address + i)
         stream.write(chr(v))
 
     context.sp -= 4
 
 
 def write_to_file(context, q):
-    _, file_id = context.store[context.sp]
+    file_id = context.store.get_value(context.sp)
     t, v1 = context.store[context.sp - 2]
-    _, v2 = context.store[context.sp - 1]
+    v2 = context.store.get_value(context.sp - 1)
 
     stream = get_stream(context, file_id)
     if q == 10 and t != 'CHAR':
@@ -107,8 +107,8 @@ def write_to_file(context, q):
 
 
 def read_byte(context, q):
-    _, file_id = context.store[context.sp]
-    _, store_addr = context.store[context.sp - 1]
+    file_id = context.store.get_value(context.sp)
+    store_addr = context.store.get_value(context.sp - 1)
 
     stream = get_stream(context, file_id)
 
@@ -124,7 +124,7 @@ def read_byte(context, q):
 
 
 def file_eof(context):
-    _, file_id = context.store[context.sp]
+    file_id = context.store.get_value(context.sp)
     stream = get_stream(context, file_id)
     context.store[context.sp] = ('BOOL', stream.eof())
 
@@ -135,20 +135,20 @@ def call_sp(q, context: Context):
     elif q == 1:  # (*PUT*)
         putfile(context)
     elif q == 2:  # (*RST*)
-        _, value = context.store[context.sp]
+        value = context.store.get_value(context.sp)
         context.np = value
         context.sp -= 1
     elif q == 3:  # (*RLN*)
         readline(context)
     elif q == 4:  # (*NEW*)
-        _, adr = context.store[context.sp]
+        adr = context.store.get_value(context.sp)
         ad = context.np - adr
         if ad <= context.sp:
             raise RuntimeError("Store Overflow")
         for i in range(context.np - 1, ad, -1):
             context.store[i] = ('UNDEF', 0)
         context.np = ad
-        _, ad = context.store[context.sp - 1]
+        ad = context.store.get_value(context.sp - 1)
         context.store[ad] = ('ADR', context.np)
         context.sp -= 2
     elif q == 5:  # (*WLN*)
@@ -162,22 +162,22 @@ def call_sp(q, context: Context):
     elif q in (11, 12, 13):  # (*RDI*) (*RDR*) (*RDC*)
         read_byte(context, q)
     elif q == 14:  # (*SIN*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', sin(v))
     elif q == 15:  # (*COS*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', cos(v))
     elif q == 16:  # (*EXP*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', exp(v))
     elif q == 17:  # (*LOG*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', log(v))
     elif q == 18:  # (*SQT*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', sqrt(v))
     elif q == 19:  # (*ATN*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', atan(v))
     elif q == 20:  # (*SAV*)
         t, addr = context.store[context.sp]
@@ -229,7 +229,7 @@ def ex0(op, p, q, context):
         push(context)
         context.store[context.sp] = context.store[q]
     elif op == 9:  # (*IND*)
-        _, adr = context.store[context.sp]
+        adr = context.store.get_value(context.sp)
         adr += q
         t, value = context.store[adr]
         if t == 'UNDEF':
@@ -264,8 +264,8 @@ def ex0(op, p, q, context):
             context.sp = context.mp - 1
         else:
             context.sp = context.mp
-        _, pc = context.store[context.mp + 3]
-        _, mp = context.store[context.mp + 2]
+        pc = context.store.get_value(context.mp + 3)
+        mp = context.store.get_value(context.mp + 2)
         context.pc = pc
         context.mp = mp
     elif op == 15:  # (*CSP*)
@@ -275,13 +275,13 @@ def ex0(op, p, q, context):
 
 
 def compare(context, q):
-    _, adr1 = context.store[context.sp]
-    _, adr2 = context.store[context.sp + 1]
+    adr1 = context.store.get_value(context.sp)
+    adr2 = context.store.get_value(context.sp + 1)
     i = 0
     b = True
     while b and i != q:
-        _, v1 = context.store[adr1 + i]
-        _, v2 = context.store[adr2 + i]
+        v1 = context.store.get_value(adr1 + i)
+        v2 = context.store.get_value(adr2 + i)
         if v1 == v2:
             i += 1
         else:
@@ -292,14 +292,14 @@ def compare(context, q):
 def ex1(op, p, q, context):
     if op == 16:  # (*IXA*)
         context.sp -= 1
-        _, adr1 = context.store[context.sp + 1]
-        _, adr2 = context.store[context.sp]
+        adr1 = context.store.get_value(context.sp + 1)
+        adr2 = context.store.get_value(context.sp)
         context.store[context.sp] = ('ADR', q * adr1 + adr2)
     if op == 17:  # (*EQU*)
         context.sp -= 1
         if p in (0, 1, 2, 3, 4):
-            _, v1 = context.store[context.sp]
-            _, v2 = context.store[context.sp + 1]
+            v1 = context.store.get_value(context.sp)
+            v2 = context.store.get_value(context.sp + 1)
             context.store[context.sp] = ('BOOL', v1 == v2)
         elif p == 5:
             b, _ = compare(context, q)
@@ -307,8 +307,8 @@ def ex1(op, p, q, context):
     if op == 18:  # (*NEQ*)
         context.sp -= 1
         if p in (0, 1, 2, 3, 4):
-            _, v1 = context.store[context.sp]
-            _, v2 = context.store[context.sp + 1]
+            v1 = context.store.get_value(context.sp)
+            v2 = context.store.get_value(context.sp + 1)
             context.store[context.sp] = ('BOOL', v1 != v2)
         elif p == 5:
             b, _ = compare(context, q)
@@ -316,85 +316,85 @@ def ex1(op, p, q, context):
     if op == 19:  # (*GEQ*)
         context.sp -= 1
         if p in (0, 1, 2, 3, 4):
-            _, v1 = context.store[context.sp]
-            _, v2 = context.store[context.sp + 1]
+            v1 = context.store.get_value(context.sp)
+            v2 = context.store.get_value(context.sp + 1)
             context.store[context.sp] = ('BOOL', v1 >= v2)
         elif p == 5:
             b, (i1, i2) = compare(context, q)
-            _, v1 = context.store[i1]
-            _, v2 = context.store[i2]
+            v1 = context.store.get_value(i1)
+            v2 = context.store.get_value(i2)
             context.store[context.sp] = ('BOOL', v1 >= v2 or b)
     if op == 20:  # (*GRT*)
         context.sp -= 1
         if p in (0, 1, 2, 3, 4):
-            _, v1 = context.store[context.sp]
-            _, v2 = context.store[context.sp + 1]
+            v1 = context.store.get_value(context.sp)
+            v2 = context.store.get_value(context.sp + 1)
             context.store[context.sp] = ('BOOL', v1 > v2)
         elif p == 5:
             b, (i1, i2) = compare(context, q)
-            _, v1 = context.store[i1]
-            _, v2 = context.store[i2]
+            v1 = context.store.get_value(i1)
+            v2 = context.store.get_value(i2)
             context.store[context.sp] = ('BOOL', v1 > v2 and not b)
     if op == 21:  # (*LEQ*)
         context.sp -= 1
         if p in (0, 1, 2, 3, 4):
-            _, v1 = context.store[context.sp]
-            _, v2 = context.store[context.sp + 1]
+            v1 = context.store.get_value(context.sp)
+            v2 = context.store.get_value(context.sp + 1)
             context.store[context.sp] = ('BOOL', v1 <= v2)
         elif p == 5:
             b, (i1, i2) = compare(context, q)
-            _, v1 = context.store[i1]
-            _, v2 = context.store[i2]
+            v1 = context.store.get_value(i1)
+            v2 = context.store.get_value(i2)
             context.store[context.sp] = ('BOOL', v1 <= v2 or b)
     if op == 22:  # (*LES*)
         context.sp -= 1
         if p in (0, 1, 2, 3, 4):
-            _, v1 = context.store[context.sp]
-            _, v2 = context.store[context.sp + 1]
+            v1 = context.store.get_value(context.sp)
+            v2 = context.store.get_value(context.sp + 1)
             context.store[context.sp] = ('BOOL', v1 < v2)
         elif p == 5:
             b, (i1, i2) = compare(context, q)
-            _, v1 = context.store[i1]
-            _, v2 = context.store[i2]
+            v1 = context.store.get_value(i1)
+            v2 = context.store.get_value(i2)
             context.store[context.sp] = ('BOOL', v1 < v2 and not b)
     if op == 23:  # (*UJP*)
         context.pc = q
     if op == 24:  # (*FJP*)
-        _, b = context.store[context.sp]
+        b = context.store.get_value(context.sp)
         if not b:
             context.pc = q
         context.sp -= 1
     if op == 25:  # (*XJP*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.pc = v + q
         context.sp -= 1
     if op == 26:  # (*CHK*)
-        _, v1 = context.store[context.sp]
-        _, lower_bound = context.store[q - 1]
-        _, upper_bound = context.store[q]
+        v1 = context.store.get_value(context.sp)
+        lower_bound = context.store.get_value(q - 1)
+        upper_bound = context.store.get_value(q)
         if v1 < lower_bound or v1 > upper_bound:
             raise RuntimeError("Value out of range")
     if op == 27:  # (*EOF*)
         file_eof(context)
     if op == 28:  # (*ADI*)
         context.sp -= 1
-        _, v1 = context.store[context.sp]
-        _, v2 = context.store[context.sp + 1]
+        v1 = context.store.get_value(context.sp)
+        v2 = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('INT', v1 + v2)
     if op == 29:  # (*ADR*)
         context.sp -= 1
-        _, v1 = context.store[context.sp]
-        _, v2 = context.store[context.sp + 1]
+        v1 = context.store.get_value(context.sp)
+        v2 = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('REEL', v1 + v2)
     if op == 30:  # (*SBI*)
         context.sp -= 1
-        _, v1 = context.store[context.sp]
-        _, v2 = context.store[context.sp + 1]
+        v1 = context.store.get_value(context.sp)
+        v2 = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('INT', v1 - v2)
     if op == 31:  # (*SBR*)
         context.sp -= 1
-        _, v1 = context.store[context.sp]
-        _, v2 = context.store[context.sp + 1]
+        v1 = context.store.get_value(context.sp)
+        v2 = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('REEL', v1 - v2)
 
     return True
@@ -402,62 +402,62 @@ def ex1(op, p, q, context):
 
 def ex2(op, p, q, context):
     if op == 32:  # (*SGS*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('SETT', {v})
     if op == 33:  # (*FLT*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', v)
     if op == 34:  # (*FLO*)
-        _, v = context.store[context.sp - 1]
+        v = context.store.get_value(context.sp - 1)
         context.store[context.sp - 1] = ('REEL', v)
     if op == 35:  # (*TRC*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('INT', int(trunc(v)))
     if op == 36:  # (*NGI*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('INT', -v)
     if op == 37:  # (*NGR*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', -v)
     if op == 38:  # (*SQI*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('INT', v * v)
     if op == 39:  # (*SQR*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', v * v)
     if op == 40:  # (*ABI*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('INT', abs(v))
     if op == 41:  # (*ABR*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('REEL', abs(v))
     if op == 42:  # (*NOT*)
-        _, v = context.store[context.sp]
+        v = context.store.get_value(context.sp)
         context.store[context.sp] = ('BOOL', not v)
     if op == 43:  # (*AND*)
         context.sp -= 1
-        _, v1 = context.store[context.sp]
-        _, v2 = context.store[context.sp + 1]
+        v1 = context.store.get_value(context.sp)
+        v2 = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('BOOL', v1 and v2)
     if op == 44:  # (*IOR*)
         context.sp -= 1
-        _, v1 = context.store[context.sp]
-        _, v2 = context.store[context.sp + 1]
+        v1 = context.store.get_value(context.sp)
+        v2 = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('BOOL', v1 or v2)
     if op == 45:  # (*DIF*)
         context.sp -= 1
-        _, v1 = context.store[context.sp]
-        _, v2 = context.store[context.sp + 1]
+        v1 = context.store.get_value(context.sp)
+        v2 = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('SETT', v1.difference(v2))
     if op == 46:  # (*INT*)
         context.sp -= 1
-        _, v1 = context.store[context.sp]
-        _, v2 = context.store[context.sp + 1]
+        v1 = context.store.get_value(context.sp)
+        v2 = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('SETT', v1.intersection(v2))
     if op == 47:  # (*UNI*)
         context.sp -= 1
-        _, v1 = context.store[context.sp]
-        _, v2 = context.store[context.sp + 1]
+        v1 = context.store.get_value(context.sp)
+        v2 = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('SETT', v1.union(v2))
 
     return True
@@ -466,40 +466,40 @@ def ex2(op, p, q, context):
 def ex3(op, p, q, context):
     if op == 48:  # (*INN*)
         context.sp -= 1
-        _, int_value = context.store[context.sp]
-        _, set_value = context.store[context.sp + 1]
+        int_value = context.store.get_value(context.sp)
+        set_value = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('BOOL', int_value in set_value)
     elif op == 49:  # (*MOD*)
         context.sp -= 1
-        _, a_value = context.store[context.sp]
-        _, b_value = context.store[context.sp + 1]
+        a_value = context.store.get_value(context.sp)
+        b_value = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('INT', a_value % b_value)
     elif op == 50:  # (*ODD*)
-        _, a_value = context.store[context.sp]
+        a_value = context.store.get_value(context.sp)
         context.store[context.sp] = ('BOOL', (a_value % 2) > 0)
     elif op == 51:  # (*MPI*)
         context.sp -= 1
-        _, a_value = context.store[context.sp]
-        _, b_value = context.store[context.sp + 1]
+        a_value = context.store.get_value(context.sp)
+        b_value = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('INT', a_value * b_value)
     elif op == 52:  # (*MPR*)
         context.sp -= 1
-        _, a_value = context.store[context.sp]
-        _, b_value = context.store[context.sp + 1]
+        a_value = context.store.get_value(context.sp)
+        b_value = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('REEL', a_value * b_value)
     elif op == 53:  # (*DVI*)
         context.sp -= 1
-        _, a_value = context.store[context.sp]
-        _, b_value = context.store[context.sp + 1]
+        a_value = context.store.get_value(context.sp)
+        b_value = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('INT', a_value // b_value)
     elif op == 54:  # (*DVR*)
         context.sp -= 1
-        _, a_value = context.store[context.sp]
-        _, b_value = context.store[context.sp + 1]
+        a_value = context.store.get_value(context.sp)
+        b_value = context.store.get_value(context.sp + 1)
         context.store[context.sp] = ('REEL', a_value / b_value)
     elif op == 55:  # (*MOV*)
-        _, i1_value = context.store[context.sp - 1]
-        _, i2_value = context.store[context.sp]
+        i1_value = context.store.get_value(context.sp - 1)
+        i2_value = context.store.get_value(context.sp)
         context.sp -= 2
         for i in range(q):
             context.store[i1_value + i] = context.store[i2_value + i]
@@ -509,7 +509,7 @@ def ex3(op, p, q, context):
             raise RuntimeError("Store overflow")
         context.store[context.sp] = ('ADR', q)
     elif op == 57:  # (*DEC*)
-        _, value = context.store[context.sp]
+        value = context.store.get_value(context.sp)
         context.store[context.sp] = ('INT', value - q)
     if op == 58:  # (*STP*)
         return False
